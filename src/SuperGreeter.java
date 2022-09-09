c0ee472a89b5cc6c5234c2451dcec69b2634df04
gottbrecht <simo79j5@stud.kea.dk>
2022-09-09 13:56:04 +0200

    added greet

--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -1,11 +1,16 @@
+import java.util.Scanner;
+
 public class SuperGreeter {
     public void start() {
-        System.out.println("Det virker!");
     }
-
     public static void main(String[] args) {
         SuperGreeter sg = new SuperGreeter();
+        System.out.println("Det virker!");
         sg.start();
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
 
     }
+
+
 }
